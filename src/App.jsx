59d037117e3cb5e0c80100601cf16fd8e30e72fa
Mahirshadid <mahir.shadid@gmail.com>
2025-09-24 11:13:29 +0600

    adding customized css

--- src/App.jsx
+++ src/App.jsx
@@ -4,69 +4,86 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import heroimg from "./assets/hero.jpg"
+import daisyuimg from './assets/daisyui.png'
+import firebaseimg from './assets/firebase.jpg'
+import gitimg from './assets/git.jpg'
+import htmlcss from './assets/htmlcss.png'
+import jsimg from './assets/JavaScript.png'
+import mongoimg from './assets/mongo.png'
+import nodeimg from './assets/node.png'
+import tailwindimg from './assets/tailwind.jpeg'
 
 function App() {
 
   return (
     <>
-      
       <div className="hero min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
-    <img
-      src={heroimg}
-      className="w-32 sm:w-1/2 md:w-2/3 lg:w-96 rounded-lg shadow-2xl"
-    />
-    <div>
-      <h1 className="text-3xl sm:text-5xl font-bold">Tools and Technologies for Internet Programming</h1>
-      <p className="py-6 text-balance">
-        In this course, we'll dive into the cool tools, tricks, and technologies that make the internet run. From building websites to exploring modern web frameworks, you'll get hands-on experience and practical skills to create awesome online applications. Let's get started!
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
+          <img
+            src={heroimg}
+            className="w-32 sm:w-1/2 md:w-2/3 lg:w-96 rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-3xl sm:text-5xl font-bold">
+              Tools and Technologies for Internet Programming
+            </h1>
+            <p className="py-6 text-balance">
+              In this course, we'll dive into the cool tools, tricks, and
+              technologies that make the internet run. From building websites to
+              exploring modern web frameworks, you'll get hands-on experience
+              and practical skills to create awesome online applications. Let's
+              get started!
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
       </div>
 
-<div className="hero min-h-screen">
-  <div className="hero-content flex-col lg:flex-row">
-    <div className="text-left max-w-lg">
-      <h1 className="text-3xl sm:text-5xl font-bold">
-        What we have here?
-      </h1>
-      <p className="py-6">
-        Explore the tools, frameworks, and techniques that power modern web
-        applications. <i>(Swipe the images to find out the Topics)</i>
-      </p>
-    </div>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="text-left max-w-lg">
+            <h1 className="text-3xl sm:text-5xl font-bold">
+              What we have here?
+            </h1>
+            <p className="py-6">
+              Explore the tools, frameworks, and techniques that power modern
+              web applications. <i>(Swipe the images to find out the Topics)</i>
+            </p>
+          </div>
 
-    <div className="carousel rounded-box w-64 sm:w-96">
-      <div className="carousel-item">
-        <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="1" />
+          <div className="carousel rounded-box w-64 sm:w-96">
+            <div className="carousel-item">
+              <img src={htmlcss} alt="1" />
+            </div>
+            <div className="carousel-item">
+              <img src={jsimg} alt="2" />
+            </div>
+            <div className="carousel-item">
+              <img src={tailwindimg} alt="3" />
+            </div>
+            <div className="carousel-item">
+              <img src={gitimg} alt="4" />
+            </div>
+            <div className="carousel-item">
+              <img src={firebaseimg} alt="5" />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+                alt="6"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+                alt="7"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="carousel-item">
-        <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" alt="2" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" alt="3" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" alt="4" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" alt="5" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" alt="6" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" alt="7" />
-      </div>
-    </div>
-  </div>
-</div>
-
-       
     </>
-  )
+  );
 }
 
 export default App
