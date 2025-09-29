60b55dbdcc3ec32f30ce21cc781e8c0fe178714a
Mahirshadid <mahir.shadid@gmail.com>
2025-09-29 19:09:32 +0600

    adding html page link

--- src/pages/Htmlpage.jsx
+++ src/pages/Htmlpage.jsx
@@ -4,8 +4,8 @@ const Htmlpage = () => {
   return (
     <div className='hero min-h-screen bg-base-200'>
       <div className="hero-content flex flex-col items-center max-w-3xl">
-        <h3 className='text-4xl font-bold'>HTML - Hyper Text Markup Language</h3>
-        <ul className='flex flex-col items-center' id='inner_lists'>
+        <h3 className='text-4xl font-bold text-center hover:text-cyan-400'><a href='https://www.w3schools.com/html/' target='_blank'>HTML - Hyper Text Markup Language</a></h3>
+        <ul className='flex flex-col items-center text-center' id='inner_lists'>
             <li>Introduction to HTML</li>
             <li>Basics of HTML</li>
             <li>HTML Tags</li>
